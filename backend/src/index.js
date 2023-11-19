14b7c685afe3a9cefdaafd12dbe691b4ff39cb7c
itric <itric46@gmail.com>
2023-11-19 13:54:01 +0545

    db set up

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+// import booksRoute from "./routes/booksRoute.js";
+// import { PORT, mongoDBURL } from "./config.js";
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// PORT = 8001;
+mongoose
+  .connect(process.env.mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(8001, () => {
+      console.log(`App is listening to port: ${8001}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
